c82905de7a4ee77d468a18a2eebf3be63a0fe2ae
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:26:48 -0500

    are the files here?

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 
 def main():
     alias_id_file_name = os.environ.get('INPUT_ALIAS_ID_FILE_NAME')
+    path_to_files = os.environ.get('GITHUB_WORKSPACE')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
     modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
 
@@ -12,7 +13,7 @@ def main():
     # with open(alias_id_file_name, 'r') as f:
     #     alias_ids = json.loads(f.read())
 
-    print(alias_id_file_name, modified_files, os.listdir('/'))
+    print(alias_id_file_name, modified_files, os.listdir(path_to_files))
 
 
 if __name__ == '__main__':

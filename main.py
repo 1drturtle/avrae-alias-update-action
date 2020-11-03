3c656cdda88db6aeea31716aaf86ccf02631b5bf
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:30:10 -0500

    start doing actual code

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ def main():
     alias_id_file_name = os.environ.get('INPUT_ALIAS_ID_FILE_NAME')
     path_to_files = os.environ.get('GITHUB_WORKSPACE')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
-    modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
+    modified_files = json.loads(os.environ.get('INPUT_MODIFIED-FILES', '[]'))
 
-    # alias_ids = {}
-    # with open(alias_id_file_name, 'r') as f:
-    #     alias_ids = json.loads(f.read())
+    alias_ids = {}
+    with open(path_to_files + '/' + alias_id_file_name, 'r') as f:
+        alias_ids = json.loads(f.read())
 
-    print(alias_id_file_name, modified_files, os.listdir(path_to_files))
+    to_publish = []
+    for filename in os.listdir(path_to_files):
+        if filename.startswith('.'):
+            continue
+        to_publish.append(filename)
+
+    print(alias_ids, modified_files, os.listdir(path_to_files), to_publish)
 
 
 if __name__ == '__main__':

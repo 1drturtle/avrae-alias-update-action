80011db409e779ac35550422fa273d864bd06421
BuildTools <16567386+1drturtle@users.noreply.github.com>
2020-11-03 15:34:36 -0500

    why is json not parsing?

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     alias_id_file_name = os.environ.get('INPUT_ALIAS_ID_FILE_NAME')
     path_to_files = os.environ.get('GITHUB_WORKSPACE')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
-    modified_files = json.loads(os.getenv('INPUT_MODIFIED-FILES', '[]'))
+    modified_files = os.getenv('INPUT_MODIFIED-FILES', '[]')
 
     with open(path_to_files + '/' + alias_id_file_name, 'r') as f:
         alias_ids = json.loads(f.read())

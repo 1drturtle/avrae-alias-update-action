b76341cc171c65660bc749949a9a847243e6e82e
BuildTools <16567386+1drturtle@users.noreply.github.com>
2020-11-03 15:28:04 -0500

    re-add json parsing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     alias_id_file_name = os.environ.get('INPUT_ALIAS_ID_FILE_NAME')
     path_to_files = os.environ.get('GITHUB_WORKSPACE')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
-    modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
+    modified_files = json.loads(os.environ.get('INPUT_MODIFIED-FILES', '[]'))
 
     alias_ids = {}
     with open(path_to_files + '/' + alias_id_file_name, 'r') as f:

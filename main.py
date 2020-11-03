e38338118ada1828df525e99db8dc2d57cb721a9
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 13:57:26 -0500

    See what the environs are

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def main():
     avrae_token = os.environ.get('INPUT_avrae-token')
     modified_files = json.loads(os.environ.get('INPUT_modified-files', '[]'))
 
-    alias_ids = {}
-    with open(alias_id_file_name, 'r') as f:
-        alias_ids = json.loads(f.read())
-
-    print(alias_ids)
+    # alias_ids = {}
+    # with open(alias_id_file_name, 'r') as f:
+    #     alias_ids = json.loads(f.read())
+    #
+    print(f'{alias_id_file_name}\n{avrae_token is not None}\n{modified_files}')
 
 
 if __name__ == '__main__':

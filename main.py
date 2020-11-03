2167af6209647f16856f213596f67b64283dc0f9
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:10:13 -0500

    print what we have

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import json
 def main():
     alias_id_file_name = os.environ.get('INPUT_ALIAS_IDS_FILE_NAME')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
-    modified_files = json.loads(os.environ.get('INPUT_MODIFIED-FILES', '[]'))
+    modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
 
     alias_ids = {}
     with open(alias_id_file_name, 'r') as f:
-        alias_ids = f.read()
+        alias_ids = json.loads(f.read())
 
-    print(alias_ids)
+    print(alias_ids, '\n', modified_files)
 
 
 if __name__ == '__main__':

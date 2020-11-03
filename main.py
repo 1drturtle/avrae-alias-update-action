3a9b6e493e584dbd735be8bb85e40f7ecb68767a
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:16:41 -0500

    docker is weird

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 
 def main():
-    alias_id_file_name = os.environ.get('INPUT_ALIAS_IDS_FILE_NAME')
+    alias_id_file_name = os.environ.get('INPUT_ALIAS_ID_FILE_NAME')
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
     modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
 
@@ -12,8 +12,7 @@ def main():
     # with open(alias_id_file_name, 'r') as f:
     #     alias_ids = json.loads(f.read())
 
-    print(alias_id_file_name, '\n', modified_files)
-    print(os.listdir(os.getcwd()))
+    print(alias_id_file_name, modified_files, os.listdir(os.path.dirname(os.path.realpath(__file__))))
 
 
 if __name__ == '__main__':

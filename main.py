f4dfdae83516d133c72ac0a470bee85d0c84e081
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:12:25 -0500

    where are we, what is in here

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def main():
     avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
     modified_files = os.environ.get('INPUT_MODIFIED-FILES', '[]')
 
-    alias_ids = {}
-    with open(alias_id_file_name, 'r') as f:
-        alias_ids = json.loads(f.read())
+    # alias_ids = {}
+    # with open(alias_id_file_name, 'r') as f:
+    #     alias_ids = json.loads(f.read())
 
-    print(alias_ids, '\n', modified_files)
+    print(alias_id_file_name, '\n', modified_files)
+    print(os.listdir(os.getcwd()))
 
 
 if __name__ == '__main__':

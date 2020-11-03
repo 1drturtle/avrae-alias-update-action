6788551bf93b993509a9ef88bbde2205bf79615f
Chris A <16567386+1drturtle@users.noreply.github.com>
2020-11-03 14:07:18 -0500

    fixed environ names, let's see how files load

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import json
 
 
 def main():
-    alias_id_file_name = os.environ.get('INPUT_alias-ids-file')
-    avrae_token = os.environ.get('INPUT_avrae-token')
-    modified_files = json.loads(os.environ.get('INPUT_modified-files', '[]'))
+    alias_id_file_name = os.environ.get('INPUT_ALIAS_IDS_FILE_NAME')
+    avrae_token = os.environ.get('INPUT_AVRAE_TOKEN')
+    modified_files = json.loads(os.environ.get('INPUT_MODIFIED-FILES', '[]'))
 
-    # alias_ids = {}
-    # with open(alias_id_file_name, 'r') as f:
-    #     alias_ids = json.loads(f.read())
-    #
-    all_environs = os.environ
-    print(all_environs)
+    alias_ids = {}
+    with open(alias_id_file_name, 'r') as f:
+        alias_ids = json.loads(f.read())
+
+    print(alias_ids)
 
 
 if __name__ == '__main__':
